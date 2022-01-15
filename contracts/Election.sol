28c0c668cecbbc3091a6a750a7e017e077e2f149
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2022-01-15 18:26:59 +0530

    added some restrictions like (double voting, candidate within the list) which is applicable to console but not in the client side(app.js)

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -16,6 +16,9 @@ contract Election {
     //store Candidate count(because we cant know the size of the mappping anfd we cant iterate over the mapping)
     uint public candidatesCount;
 
+    // store accounts that have already voted
+    mapping(address => bool) public voters;
+
     // add candidate function
     function addCandidate (string memory _name) private { //we are using private on this function because we dont want anyothers to add candidates
         candidatesCount++;
@@ -26,5 +29,25 @@ contract Election {
     constructor() public{
         addCandidate("GOKU");
         addCandidate("SAITAMA");
+    }
+
+    function vote(uint _candidateId) public {
+        // the statements below require(condition) will get executed only if the condition is 'true'
+        
+        //require that they haven't voted before 
+        require(!voters[msg.sender]); // voters[msg.sender] returns false(since the voter hasnt voted) so to make it true we use'!'
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount); // checks whether the candidate is within the candidateId list
+
+    // To access the accounts in truffle test use
+    // web3.eth.getAccounts().then(function(acc) {acounts=acc;})
+    // accounts (this will print the list)
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate  vote count
+        candidates[_candidateId].voteCount++;
     }
 }
